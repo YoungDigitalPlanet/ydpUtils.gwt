61c9d55a64857b58b534aab8aca3ef4d0282a686
rrybacki <rrybacki@3e83ccf1-c6ce-0310-9054-8fdb33cf0640>
2012-07-24 07:15:08 +0000

    add QueueSet bug 66942
    git-svn-id: http://svn.intranet.ydp/ydp/projects/empiria/utils/ydpgwtutils/trunk@112335 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ src/eu/ydp/gwtutil/client/util/QueueSet.java
@@ -0,0 +1,112 @@
+package eu.ydp.gwtutil.client.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class QueueSet<E> implements Set<E> {
+	
+	private List<E> list = new ArrayList<E>();
+
+	@Override
+	public boolean add(E e) {
+		if (list.contains(e))
+			return false;
+		list.add(e);
+		return true;
+	}
+	
+	public boolean append(E e) {
+		if (list.contains(e)){
+			list.remove(e);			
+			list.add(e);
+		}
+		list.add(e);
+		return true;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		boolean added = false;
+		for (E e : c){
+			if (!list.contains(e)){
+				list.add(e);
+				added = true;
+			}
+		}
+		return added;
+	}
+	
+	public boolean appendAll(Collection<? extends E> c) {
+		boolean added = false;
+		for (E e : c){
+			if (!list.contains(e)){
+				list.add(e);
+				added = true;
+			} else if (list.indexOf(e) != list.size() - 1) {
+				list.remove(e);
+				list.add(e);
+				added = true;
+			}
+		}
+		return added;
+	}
+
+	@Override
+	public void clear() {
+		list.clear();
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		return list.contains(o);
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		return list.containsAll(c);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return list.isEmpty();
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return list.iterator();
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		return list.remove(o);
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		return list.removeAll(c);
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		return list.retainAll(c);
+	}
+
+	@Override
+	public int size() {
+		return list.size();
+	}
+
+	@Override
+	public Object[] toArray() {
+		return list.toArray();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		return toArray(a);
+	}
+
+}
