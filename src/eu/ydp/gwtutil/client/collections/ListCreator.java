e22aaa20452a8ed9f79429c703faba51a3d83f73
rrybacki <rrybacki@3e83ccf1-c6ce-0310-9054-8fdb33cf0640>
2012-08-27 14:32:03 +0000

    add list creator bug 70098
    git-svn-id: http://svn.intranet.ydp/ydp/projects/empiria/utils/ydpgwtutils/trunk@116121 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ src/eu/ydp/gwtutil/client/collections/ListCreator.java
@@ -0,0 +1,37 @@
+package eu.ydp.gwtutil.client.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class ListCreator<T> {
+
+	private List<T> list;
+	
+	private ListCreator(){
+		list = new ArrayList<T>();
+	}
+	
+	private ListCreator(List<T> list){
+		this.list = list;
+	}
+	
+	public static <T> ListCreator<T> create(T element){
+		ListCreator<T> listCreator = new ListCreator<T>(); 
+		listCreator.list.add(element);
+		return listCreator;
+	}
+	
+	public static <T> ListCreator<T> create(List<T> list){
+		ListCreator<T> listCreator = new ListCreator<T>(list);
+		return listCreator;
+	}
+	
+	public ListCreator<T> add(T element){
+		list.add(element);
+		return this;
+	}
+	
+	public List<T> build(){
+		return list;
+	}
+}
---
+++ test/eu/ydp/gwtutil/client/collections/ListCreatorTest.java
@@ -0,0 +1,26 @@
+package eu.ydp.gwtutil.client.collections;
+
+import java.util.List;
+import java.util.Vector;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import org.junit.Test;
+
+public class ListCreatorTest {
+
+	@Test
+	public void testCreate(){
+		List<Integer> list = ListCreator.create(2).add(3).add(4).build();
+		assertThat(list.size(), equalTo(3));
+		assertThat(list, contains(2, 3, 4));
+	}
+
+	@Test
+	public void testImpl(){
+		List<Integer> list = ListCreator.create(new Vector<Integer>()).add(2).add(3).add(4).build();
+		assertThat(list.size(), equalTo(3));
+		assertThat(list, contains(2, 3, 4));
+		assertThat(list.getClass().getName(), equalTo(Vector.class.getName()));
+	}
+}
