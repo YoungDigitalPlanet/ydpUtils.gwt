df543e6ab0cdac5f1df9c4cbf5620090b4b74d5a
plelakowski <plelakowski@3e83ccf1-c6ce-0310-9054-8fdb33cf0640>
2012-09-13 10:15:00 +0000

    add option to disable page change by swipe bug 71184
    git-svn-id: http://svn.intranet.ydp/ydp/projects/empiria/utils/ydpgwtutils/trunk@117774 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ src/eu/ydp/gwtutil/client/ui/GWTPanelFactory.java
@@ -0,0 +1,12 @@
+package eu.ydp.gwtutil.client.ui;
+
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.FlowPanel;
+
+public interface GWTPanelFactory {
+
+	public abstract FlowPanel getFlowPanel();
+
+	public abstract AbsolutePanel getAbsolutePanel();
+
+}
---
+++ src/eu/ydp/gwtutil/client/ui/GWTPanelFactoryImpl.java
@@ -0,0 +1,24 @@
+package eu.ydp.gwtutil.client.ui;
+
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.FlowPanel;
+
+public class GWTPanelFactoryImpl implements GWTPanelFactory {
+
+	/* (non-Javadoc)
+	 * @see eu.ydp.gwtutil.client.ui.GWTPanelFactory#getFlowPanel()
+	 */
+	@Override
+	public FlowPanel getFlowPanel(){
+		return new FlowPanel();
+	}
+
+	/* (non-Javadoc)
+	 * @see eu.ydp.gwtutil.client.ui.GWTPanelFactory#getAbsolutePanel()
+	 */
+	@Override
+	public AbsolutePanel getAbsolutePanel(){
+		return new AbsolutePanel();
+	}
+
+}
