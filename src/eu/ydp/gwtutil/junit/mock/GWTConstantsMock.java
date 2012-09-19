20a6c9ce938d7fd221d93d2f30a166e55adcb879
plelakowski <plelakowski@3e83ccf1-c6ce-0310-9054-8fdb33cf0640>
2012-09-19 10:20:05 +0000

    template parser test
    git-svn-id: http://svn.intranet.ydp/ydp/projects/empiria/utils/ydpgwtutils/trunk@118367 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ src/eu/ydp/gwtutil/junit/mock/GWTConstantsMock.java
@@ -0,0 +1,43 @@
+package eu.ydp.gwtutil.junit.mock;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.mockito.Mockito;
+
+import com.google.gwt.i18n.client.Constants;
+import com.google.gwt.i18n.client.Constants.DefaultStringValue;
+
+/**
+ * Mock all method from class that implement {@link Constants}
+ *
+ * @author plelakowski
+ *
+ */
+public class GWTConstantsMock {
+	/**
+	 * Mockuje wszystkie medoy oznaczone annotacja {@link DefaultStringValue}
+	 *
+	 * @param mock
+	 * @return mocka
+	 */
+	public static <T> T mockAllStringMethods(T mock) {
+		Method[] methods = mock.getClass().getMethods();
+		for (Method method : methods) {
+			try {
+				if (method.isAnnotationPresent(DefaultStringValue.class)) {
+					DefaultStringValue ann = method.getAnnotation(DefaultStringValue.class);
+					Mockito.when(method.invoke(mock, null)).thenReturn(ann.value());
+				}
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+			}
+		}
+		return mock;
+	}
+
+}
