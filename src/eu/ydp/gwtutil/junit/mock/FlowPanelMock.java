d45e2f9b2daaa4bb6a2e0df77a27f0b1ef4bab27
plelakowski <plelakowski@3e83ccf1-c6ce-0310-9054-8fdb33cf0640>
2012-09-19 10:29:46 +0000

    template parser test
    git-svn-id: http://svn.intranet.ydp/ydp/projects/empiria/utils/ydpgwtutils/trunk@118370 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/gwtutil/junit/mock/FlowPanelMock.java
+++ src/eu/ydp/gwtutil/junit/mock/FlowPanelMock.java
@@ -10,9 +10,9 @@ import com.google.gwt.user.client.ui.FlowPanel;
 public class FlowPanelMock {
 	public FlowPanel createFlowPanelMock(){
 		FlowPanel panel = Mockito.mock(FlowPanel.class);
-		ElementMock em = new ElementMock();
-		com.google.gwt.user.client.Element element = em.createElementMock();
-		Mockito.when(panel.getElement()).thenReturn(element);
+	//	ElementMock em = new ElementMock();
+	//	com.google.gwt.user.client.Element element = em.createElementMock();
+	//	Mockito.when(panel.getElement()).thenReturn(element);
 		return panel;
 	}
 }
