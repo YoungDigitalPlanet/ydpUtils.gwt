c6bf2df4cd75371e02bcd883ad63c2f0bc249f09
plelakowski <plelakowski@3e83ccf1-c6ce-0310-9054-8fdb33cf0640>
2012-10-19 07:20:44 +0000

    Remove gwt dependency
    git-svn-id: http://svn.intranet.ydp/ydp/projects/empiria/utils/ydpgwtutils/trunk@121604 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ test/eu/ydp/gwtutil/client/collections/RandomizedSetTest.java
@@ -0,0 +1,27 @@
+package eu.ydp.gwtutil.client.collections;
+
+import static org.junit.Assert.assertFalse;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class RandomizedSetTest {
+	@Test
+	public void randomSetTest(){
+		//prepare
+
+		RandomizedSet<Integer> randomizedSet = new RandomizedSet<Integer>();
+		int size = 10;
+		Integer[] source = new Integer[size],result = new Integer[size];
+		for(int x=0; x < size;++x){
+			source[x]=x;
+			randomizedSet.push(Integer.valueOf(x));
+		}
+		for(int x=0; x < size;++x){
+			result[x]=randomizedSet.pull();
+		}
+		//test
+		assertFalse(Arrays.equals(source, result));
+	}
+}
