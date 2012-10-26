a3b16b96b1f7d88c0761075c58346b07040fa5cb
plelakowski <plelakowski@3e83ccf1-c6ce-0310-9054-8fdb33cf0640>
2012-10-26 10:24:26 +0000

    regex matcher util bug 71304
    git-svn-id: http://svn.intranet.ydp/ydp/projects/empiria/utils/ydpgwtutils/trunk@122481 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ src/eu/ydp/gwtutil/client/regex/RegexMatcher.java
@@ -0,0 +1,10 @@
+package eu.ydp.gwtutil.client.regex;
+
+import com.google.gwt.regexp.shared.RegExp;
+
+public class RegexMatcher {
+	public boolean matches(String string, String regex){
+		RegExp pattern = RegExp.compile(regex);
+		 return pattern.test(string);
+	}
+}
---
+++ test/eu/ydp/gwtutil/client/regex/RegexMatcherTest.java
@@ -0,0 +1,34 @@
+package eu.ydp.gwtutil.client.regex;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class RegexMatcherTest {
+	@Test
+	public void matchTest() {
+		RegexMatcher matcher = new RegexMatcher();
+		assertTrue(matcher.matches("abbbbcd", ""));
+		assertTrue(matcher.matches("abbbbcd", "ab*bcd"));
+		assertTrue(matcher.matches("abbbbcd", "b*"));
+		assertTrue(matcher.matches("abbbbcd", "ab+cd"));
+		assertTrue(matcher.matches("abbbbcd", "b+"));
+		assertTrue(matcher.matches("abbbbcd", "ab+bcd"));
+		assertTrue(matcher.matches("abbbbcd", "ab*bcd"));
+
+		assertTrue(matcher.matches("abbbbcd", "ab+cd"));
+		assertTrue(matcher.matches("abbbbcd", "a.*d"));
+		assertTrue(matcher.matches("abbbbcd", "ab+bcd"));
+		assertFalse(matcher.matches("abbbbcd", "a.*e"));
+
+
+		assertTrue(matcher.matches("abbbbcd", "a.*d"));
+		assertTrue(matcher.matches("", "(|none)"));
+		assertFalse(matcher.matches("abbbbcd", "a.*e"));
+		assertTrue(matcher.matches("none", "(|none)"));
+		assertTrue(matcher.matches("none", "^(|none)$"));
+		assertFalse(matcher.matches("abbbbcd", "^b*$"));
+
+	}
+}
