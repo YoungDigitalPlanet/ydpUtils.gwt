ad0d77d0edf3c802e99a2f48e68a3f11ea567484
plelakowski <plelakowski@3e83ccf1-c6ce-0310-9054-8fdb33cf0640>
2012-11-13 08:14:39 +0000

    runner that modify class to enable mocking final/native method by mackito bug 74201
    git-svn-id: http://svn.intranet.ydp/ydp/projects/empiria/utils/ydpgwtutils/trunk@124017 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ src/eu/ydp/gwtutil/junit/runners/PrepareForTest.java
@@ -0,0 +1,12 @@
+package eu.ydp.gwtutil.junit.runners;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface PrepareForTest {
+	 Class<?>[] value() default {};
+}
