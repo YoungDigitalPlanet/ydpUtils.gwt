39fd5719c8cf42250a0a33596c1eabb2f89a16ca
rrybacki <rrybacki@3e83ccf1-c6ce-0310-9054-8fdb33cf0640>
2012-11-20 08:57:40 +0000

    refactoring and comment
    git-svn-id: http://svn.intranet.ydp/ydp/projects/empiria/utils/ydpgwtutils/trunk@124686 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/gwtutil/client/constants/StringConstants.java
+++ src/eu/ydp/gwtutil/client/constants/StringConstants.java
@@ -1,6 +1,44 @@
 package eu.ydp.gwtutil.client.constants;
 
+/**
+ * Allow for dynamic creation of string constants using GWT generators.
+ * 
+ * Sample implementation:
+ * 
+ * <pre>
+ * {@code
+ * @Prefix("pref-")
+ * @Suffix("-suf")
+ * @Separator("-")  // defines characters separating components
+ * public interface MyConstants extends StringConstants {
+ * 		MyConstants first();
+ * 		MyConstants second();
+ * }
+ * 
+ * // ...
+ * 
+ * String s1 = myConstants.first().toString(); // will return "pref-first-suf"
+ * String s2 = myConstants.first().second().toString(); // will return "pref-first-second-suf"
+ * 
+ * }
+ * </pre>
+ *
+ * @author rrybacki
+ * 
+ * @see Prefix
+ * @see Suffix
+ * @see Separator
+ * @see Output
+ */
 public interface StringConstants {
 
+	/**
+	 * Call this method as the exit from string constant builder chain.
+	 * 
+	 * Ex. <code>String output =  stringConstantsImpl.aaa().bbb().toString();</code>
+	 * 
+	 * @return Built string containing desired string constant
+	 */
+	@Override
 	String toString();
 }
