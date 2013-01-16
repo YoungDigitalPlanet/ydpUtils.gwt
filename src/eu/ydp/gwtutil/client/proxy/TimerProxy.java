2b7672d557e6b2424593e498a3c9062cb1b40fd2
rrybacki <rrybacki@3e83ccf1-c6ce-0310-9054-8fdb33cf0640>
2013-01-16 09:11:13 +0000

    add GWT Timer Proxy issue EMPCP-181
    git-svn-id: http://svn.intranet.ydp/ydp/projects/empiria/utils/ydpgwtutils/trunk@130057 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ src/eu/ydp/gwtutil/client/proxy/TimerProxy.java
@@ -0,0 +1,70 @@
+package eu.ydp.gwtutil.client.proxy;
+
+import com.google.gwt.user.client.Timer;
+
+/**
+ * Proxy class for {@link Timer}. Allows easy mocking.
+ * 
+ * Additionally {@link #schedule(int)} accepts non-positive delay value.
+ * 
+ * @author rrybacki
+ *
+ */
+public abstract class TimerProxy {
+
+	Timer timer;
+
+	/**
+	 * @see Timer#run()
+	 */
+	public abstract void run();
+
+	/**
+	 * Proxy for {@link Timer#schedule(int)}. 
+	 * 
+	 * Accepts non-positive delay value - in such case {@link #run()} is called immediately.
+	 * @param delayMillis Delay in ms or non-positive value for immediate execution.
+	 * @see Timer#schedule(int)
+	 */
+	public void schedule(int delayMillis) {
+		if (delayMillis > 0){
+			ensureTimer();
+			timer.schedule(delayMillis);
+		} else {
+			run();
+		}
+	}
+
+	/**
+	 * Proxy for {@link Timer#scheduleRepeating(int)}.
+	 * @see Timer#scheduleRepeating(int)
+	 */
+	public void scheduleRepeating(int periodMillis) {
+		ensureTimer();
+		timer.scheduleRepeating(periodMillis);
+	}
+
+	/**
+	 * @see Timer#cancel()
+	 */
+	public void cancel() {
+		if (timer != null){
+			timer.cancel();
+		}
+	}
+	
+	/**
+	 * Lazy initialization of the timer.
+	 */
+	void ensureTimer(){
+		if (timer == null){
+			timer = new Timer() {
+	
+				@Override
+				public void run() {
+					TimerProxy.this.run();
+				}
+			};
+		}
+	}
+}
