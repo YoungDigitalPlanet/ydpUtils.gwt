dda72f492be82de5ce831317c6a792c9a3ba974f
plelakowski <plelakowski@3e83ccf1-c6ce-0310-9054-8fdb33cf0640>
2013-03-28 13:22:20 +0000

    chrome/safari useragent bug YPUB-5147
    git-svn-id: http://svn.intranet.ydp/ydp/projects/empiria/utils/ydpgwtutils/trunk@136263 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/gwtutil/junit/mock/UserAgentCheckerNativeInterfaceMock.java
+++ src/eu/ydp/gwtutil/junit/mock/UserAgentCheckerNativeInterfaceMock.java
@@ -12,6 +12,7 @@ public class UserAgentCheckerNativeInterfaceMock {
 	public static final String FIREFOX_WINDOWS = "Mozilla/5.0 (Windows NT 6.1; rv:15.0) Gecko/20120716 Firefox/15.0a2";
 	public static final String FIREFOX_ANDROID = "Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30";
 	public static final String IE_9 ="Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; en-US))";
+	public static final String SAFARI = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.172 Safari/537.22";
 	public static BrowserNativeInterface getNativeInterfaceMock(String userAgent) {
 		BrowserNativeInterface nativeInterface = Mockito.mock(BrowserNativeInterface.class);
 		Mockito.when(nativeInterface.getUserAgentStrting()).thenReturn(userAgent);
