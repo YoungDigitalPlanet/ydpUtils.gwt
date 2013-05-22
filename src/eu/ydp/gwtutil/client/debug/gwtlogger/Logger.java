ed9ecdc4a4335b88d3a1baca97da1ead2fd3283c
plelakowski <plelakowski@3e83ccf1-c6ce-0310-9054-8fdb33cf0640>
2013-05-22 16:10:19 +0000

    Extends logger to handle asserts on logging
    git-svn-id: http://svn.intranet.ydp/ydp/projects/empiria/utils/ydpgwtutils/trunk@140420 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/gwtutil/client/debug/gwtlogger/Logger.java
+++ src/eu/ydp/gwtutil/client/debug/gwtlogger/Logger.java
@@ -2,46 +2,71 @@ package eu.ydp.gwtutil.client.debug.gwtlogger;
 
 
 public class Logger implements ILogger {
-	
+
 	java.util.logging.Logger  logger;
-	
+
 	public Logger(){
 		this.logger = java.util.logging.Logger.getLogger("");
 	}
-	
+
 	@Override
 	public void fine(String message) {
 		this.logger.fine(message);
-		
 	}
-	
+
+	public void fineIf(boolean azzert,String message) {
+		if(azzert){
+			fine(message);
+		}
+	}
+
 	@Override
 	public void info(String message) {
 		this.logger.info(message);
-		
+
 	}
-	
+
+	public void infoIf(boolean azzert,String message) {
+		if(azzert){
+			info(message);
+		}
+
+	}
+
 	@Override
 	public void severe(String message) {
 		this.logger.severe(message);
 	}
-	
+
+
+	public void severeIf(boolean azzert,String message) {
+		if(azzert){
+		  severe(message);
+		}
+	}
+
 	@Override
 	public void warning(String message) {
 		this.logger.warning(message);
 	}
-	
+
+	public void warningIf(boolean azzert, String message){
+		if(azzert){
+			warning(message);
+		}
+	}
+
 	public void methodLog(String className, String methodName, Object ... args){
 		StringBuilder builder = new StringBuilder();
 		builder.append("Calling: ")
 			.append(className)
 			.append(".")
 			.append(methodName);
-		
+
 		if(args != null){
 			appendArguments(builder, args);
 		}
-		
+
 		this.logger.info(builder.toString());
 	}
 
