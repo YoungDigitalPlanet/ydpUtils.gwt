5dadba84568dae56cedfe95cb6c7913d7428262e
plelakowski <plelakowski@ydp.com.pl>
2013-07-10 12:58:45 +0200

    String predicates

---
+++ src/eu/ydp/gwtutil/client/predicate/StringPredicates.java
@@ -0,0 +1,22 @@
+package eu.ydp.gwtutil.client.predicate;
+
+import javax.annotation.Nullable;
+
+import com.google.common.base.Predicate;
+import com.google.gwt.thirdparty.guava.common.base.Strings;
+
+public class StringPredicates {
+	/**
+	 * ommit empty strings and null  !Strings.isNullOrEmpty(value)
+	 * @return
+	 */
+	public static Predicate<String> notBlank(){
+		return new Predicate<String>() {
+
+			@Override
+			public boolean apply(@Nullable String value) {
+				return !Strings.isNullOrEmpty(value) && !value.trim().isEmpty();
+			}
+		};
+	}
+}
---
+++ test/eu/ydp/gwtutil/client/predicate/StringPredicatesTest.java
@@ -0,0 +1,35 @@
+package eu.ydp.gwtutil.client.predicate;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+
+public class StringPredicatesTest {
+
+	@Test
+	public void notBlankEmptyStrings() throws Exception {
+		List<String> listWithEmptyStrings = Lists.newArrayList("","");
+		Iterable<String> filtered = Iterables.filter(listWithEmptyStrings, StringPredicates.notBlank());
+		assertEquals(Lists.newArrayList(filtered).size(), 0);
+	}
+
+	@Test
+	public void notBlankNulls() throws Exception {
+		List<String> listWithEmptyStrings = Lists.newArrayList(null,null);
+		Iterable<String> filtered = Iterables.filter(listWithEmptyStrings, StringPredicates.notBlank());
+		assertEquals(Lists.newArrayList(filtered).size(), 0);
+	}
+
+	@Test
+	public void notBlankMixed() throws Exception {
+		List<String> listWithEmptyStrings = Lists.newArrayList(null,null,"","a"," ","w");
+		Iterable<String> filtered = Iterables.filter(listWithEmptyStrings, StringPredicates.notBlank());
+		assertEquals(Lists.newArrayList(filtered).size(), 2);
+	}
+
+}
