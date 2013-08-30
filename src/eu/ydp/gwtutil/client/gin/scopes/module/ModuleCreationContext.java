ca9b8f99fb8892545a6934db1d6f60611f7aa609
mkocieniewski <mkocieniewski@ydp.com.pl>
2013-08-30 16:48:14 +0200

    move ModuleScope to utils (correction) bug EMPCP-379

--- src/eu/ydp/gwtutil/client/gin/scopes/module/ModuleCreationContext.java
+++ src/eu/ydp/gwtutil/client/gin/scopes/module/ModuleCreationContext.java
@@ -1,17 +1,22 @@
 package eu.ydp.gwtutil.client.gin.scopes.module;
 
+import com.google.gwt.user.client.resource.ResourceModel;
 import com.google.gwt.xml.client.Element;
 
 public class ModuleCreationContext {
 
-	private final Element xmlElement;
+	private final ResourceModel<Element> resourceModel;
 
 	public ModuleCreationContext(Element xmlElement) {
-		this.xmlElement = xmlElement;
+		this.resourceModel = new ResourceModel<Element>(xmlElement, "");
+	}
+	
+	public ModuleCreationContext(ResourceModel<Element> resourceModel) {
+		this.resourceModel = resourceModel;
 	}
 
 	public Element getXmlElement() {
-		return xmlElement;
+		return resourceModel.getResource();
 	}
 	
 }
