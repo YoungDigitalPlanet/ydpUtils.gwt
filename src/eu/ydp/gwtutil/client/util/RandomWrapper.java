e953f19676ca4916527bcda27c0f44407346be56
plelakowski <plelakowski@ydp.com.pl>
2013-09-24 12:19:55 +0200

    YPUB-5726 : correct randomWrapper behavior

--- src/eu/ydp/gwtutil/client/util/RandomWrapper.java
+++ src/eu/ydp/gwtutil/client/util/RandomWrapper.java
@@ -3,9 +3,8 @@ package eu.ydp.gwtutil.client.util;
 import com.google.gwt.user.client.Random;
 
 public class RandomWrapper {
-	
-	public int nextInt(int n){
-		int nextInt = Random.nextInt();
-		return nextInt % n;
+
+	public int nextInt(int n) {
+		return Random.nextInt(n);
 	}
 }
