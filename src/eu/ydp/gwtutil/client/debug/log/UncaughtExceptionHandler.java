01e9fc540c66d3fc20f30d6a8573330dffa37865
Jacek Jackowiak <jjackowiak@ydp.com.pl>
2013-11-29 08:37:15 +0100

    UncaughtExceptionHandler added. YPUB-5984

---
+++ src/eu/ydp/gwtutil/client/debug/log/UncaughtExceptionHandler.java
@@ -0,0 +1,19 @@
+package eu.ydp.gwtutil.client.debug.log;
+
+import com.google.gwt.core.client.GWT;
+import com.google.inject.Inject;
+
+public class UncaughtExceptionHandler implements GWT.UncaughtExceptionHandler {
+
+	private final Logger logger;
+
+	@Inject
+	public UncaughtExceptionHandler(Logger logger) {
+		this.logger = logger;
+	}
+
+	@Override
+	public void onUncaughtException(Throwable e) {
+		logger.error(e);
+	}
+}
