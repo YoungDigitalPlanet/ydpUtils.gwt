79aca018ef0416c752d45fc368f28ecb23c87179
Jacek Jackowiak <jjackowiak@ydp.com.pl>
2014-08-14 12:45:14 +0200

    [YPUB-6517] Removing ParameterizedMethodsRunner

--- test/eu/ydp/gwtutil/client/geom/MathUtilTest.java
+++ test/eu/ydp/gwtutil/client/geom/MathUtilTest.java
@@ -1,28 +1,30 @@
 package eu.ydp.gwtutil.client.geom;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
-import java.util.Arrays;
-import java.util.Collection;
-
+import eu.ydp.gwtutil.AbstractTestBase;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import eu.ydp.gwtutil.AbstractTestBase;
-import eu.ydp.gwtutil.junit.runners.ParameterizedMethodsRunner;
-import eu.ydp.gwtutil.junit.runners.ParameterizedMethodsRunner.MethodParameters;
+import static junitparams.JUnitParamsRunner.$;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
-@RunWith(ParameterizedMethodsRunner.class)
+@RunWith(JUnitParamsRunner.class)
 public class MathUtilTest extends AbstractTestBase {
 
-	@MethodParameters(forMethod = "distance", name = "{index}: |{0},{1}| |{2},{3}| -> {4}")
-	public static Collection<Object[]> data() {
-		return Arrays.asList(new Object[][] { { 0d, 0d, 0d, 0d, 0d }, { 0d, 0d, 100d, 0d, 100d }, { 0d, 0d, 300d, 400d, 500d }, { 0d, 0d, -300d, -400d, 500d },
-				{ -200d, -200d, 100d, 200d, 500d }, });
+	public Object[] data() {
+		return $(
+				$(0d, 0d, 0d, 0d, 0d),
+				$(0d, 0d, 100d, 0d, 100d),
+				$(0d, 0d, 300d, 400d, 500d),
+				$(0d, 0d, -300d, -400d, 500d),
+				$(-200d, -200d, 100d, 200d, 500d)
+		);
 	}
 
 	@Test
+	@Parameters(method = "data")
 	public void distance(double x1, double y1, double x2, double y2, double result) {
 		assertThat(MathUtil.distance(x1, y1, x2, y2), is(result));
 	}
