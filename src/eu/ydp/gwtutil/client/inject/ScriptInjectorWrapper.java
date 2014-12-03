853a6da7c4dee9e7cca680d2a4d65ad6cc2a6f24
aleksanderkotbury <AKotynski-Buryla@ydp.pl>
2014-12-03 12:56:11 +0100

    [YPUB-6700] scrip injector fix

--- src/eu/ydp/gwtutil/client/inject/ScriptInjectorWrapper.java
+++ src/eu/ydp/gwtutil/client/inject/ScriptInjectorWrapper.java
@@ -1,10 +1,15 @@
 package eu.ydp.gwtutil.client.inject;
 
+import com.google.gwt.core.client.Callback;
 import com.google.gwt.core.client.ScriptInjector;
 
 public class ScriptInjectorWrapper {
 
-	public void fromUrl(String url) {
-		ScriptInjector.fromUrl(url);
+	public void fromUrl(final String url) {
+		ScriptInjector.fromUrl(url).inject();
+	}
+
+	public void fromUrl(String url, Callback<Void, Exception> callback) {
+		ScriptInjector.fromUrl(url).setCallback(callback).inject();
 	}
 }
