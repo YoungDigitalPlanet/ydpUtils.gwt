98752efdb0f5f013970ac31b9c1d034c25639720
Łukasz Domżalski <ldomzalski@ydp.pl>
2015-06-24 17:08:26 +0200

    [YPUB-7180] Przerzucenie SynchronousScriptsLoader do util z empiria.player

---
+++ src/main/java/eu/ydp/gwtutil/client/scripts/SynchronousScriptsLoader.java
@@ -0,0 +1,50 @@
+package eu.ydp.gwtutil.client.scripts;
+
+import com.google.gwt.core.client.Callback;
+import com.google.inject.Inject;
+
+import java.util.*;
+
+/**
+ * Created by ldomzalski on 2015-06-24.
+ */
+public class SynchronousScriptsLoader {
+
+    @Inject
+    private AsynchronousScriptsLoader asynchronousScriptsLoader;
+
+    public void injectScripts(ScriptUrl[] scripts, Callback<Void, Exception> callback) {
+
+        List<ScriptUrl> scriptsList = new ArrayList<ScriptUrl>(Arrays.asList(scripts));
+        Collections.reverse(scriptsList);
+
+        Stack<ScriptUrl> scriptsStack = new Stack<ScriptUrl>();
+        scriptsStack.addAll(scriptsList);
+        injectScript(scriptsStack, callback);
+
+    }
+
+    private void injectScript(final Stack<ScriptUrl> scriptsStack, final Callback<Void, Exception> callback) {
+        if (scriptsStack.empty()) {
+            return;
+        }
+
+        asynchronousScriptsLoader.inject(scriptsStack.pop(), new Callback<Void, Exception>() {
+            @Override
+            public void onFailure(Exception reason) {
+
+            }
+
+            @Override
+            public void onSuccess(Void result) {
+                if (!scriptsStack.empty()) {
+                    injectScript(scriptsStack, callback);
+                } else {
+                    callback.onSuccess(result);
+                }
+            }
+        });
+
+    }
+
+}
