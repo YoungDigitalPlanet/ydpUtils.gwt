e6219857bc5809a453e3570105ca44c4f914b6c4
Jacek Jackowiak <jjackowiak@ydp.com.pl>
2015-08-06 13:30:06 +0200

    [YPUB-8386] Removing ReturnsJavaBeanAnswers and BeanPropertyTester

--- src/main/java/eu/ydp/gwtutil/test/bean/BeanTestInitializationException.java
+++
@@ -1,9 +0,0 @@
-package eu.ydp.gwtutil.test.bean;
-
-@SuppressWarnings("serial")
-public class BeanTestInitializationException extends RuntimeException {
-
-    public BeanTestInitializationException(Class<?> cls) {
-        super("Error initializing bean " + cls.toString());
-    }
-}
