ea2f6c18e8f5b754fff5c2f835693c3e3b2b7774
akotynski <akotynski@ydp.eu>
2015-09-10 15:04:22 +0200

    refactor eventow

--- src/main/java/eu/ydp/gwtutil/client/event/Event.java
+++
@@ -1,9 +0,0 @@
-package eu.ydp.gwtutil.client.event;
-
-public interface Event<H, T extends Enum<T>> {
-    EventType<H, T> getAssociatedType();
-
-    Object getSource();
-
-    void dispatch(H handler);
-}
